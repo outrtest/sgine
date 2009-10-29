7cedededb41bafd6ef9c8cb558bd7193fa954125
Hans.Haggstrom <Hans.Haggstrom@localhost>
2009-10-29 20:20:54 +0000

    Removed the parser for opengl enum spec files, as they didn't contain the enum groupings we'd like to use.  Added a stub for parsing open gl docbook documents instead, from which the enums can be extracted.

---
+++ src/main/scala/com/sgine/opengl/generator/enumeration/OpenGLDocParser.scala
@@ -0,0 +1,11 @@
+package com.sgine.opengl.generator.enumeration
+
+import util.parsing.combinator.syntactical.StdTokenParsers
+
+/**
+ * Parses the functions and enumeration parameters to them from opengl documentation files.
+ */
+class OpenGLDocParser {
+
+
+}
---
+++ src/test/scala/com/sgine/opengl/generator/enumeration/OpenGLDocParserTest.scala
@@ -0,0 +1,18 @@
+package com.sgine.opengl.generator.enumeration
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+
+/**
+ * Specification for OpenGLDocParser.
+ *
+ */
+class OpenGLDocParserTest extends FlatSpec with ShouldMatchers {
+
+  
+  "OpenGLDocParser" should "produce an empty list for empty input" in (pending)
+
+  "OpenGLDocParser" should "parse a simple enum sequence" in (pending)
+
+}
