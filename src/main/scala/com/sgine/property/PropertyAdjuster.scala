506e410ad8c9eee9369ea2465f2cff3e8351852d
darkfrog26 <darkfrog26@localhost>
2009-11-09 18:42:55 +0000

    Updates to PropertyAdjuster and RepeatingUnit

--- src/main/scala/com/sgine/property/PropertyAdjuster.scala
+++ src/main/scala/com/sgine/property/PropertyAdjuster.scala
@@ -1,8 +1,14 @@
 package com.sgine.property
 
-trait PropertyAdjuster[T] extends Modifiable[T] {
+import com.sgine.Updatable;
+
+trait PropertyAdjuster[T] extends Modifiable[T] with Updatable {
 	protected var current:T
 	protected var target:T
+	
+	var animator = linearAnimator(5.0, _:Double);
+	var toDouble:Function1[T, Double] = null;
+	var fromDouble:Function1[Double, T] = null;
 	
 	abstract override def modify(value:T):T = {
 		var v = super.modify(value);
@@ -13,4 +19,36 @@ trait PropertyAdjuster[T] extends Modifiable[T] {
 	}
 	
 	def adjust(source:T, destination:T, percentage:Double):T
+	
+	abstract override def update(time:Double) = {
+		super.update(time);
+		
+		if (current != target) {
+			if (toDouble == null) {
+				// TODO: find valid function
+			}
+			if (fromDouble == null) {
+				// TODO: find valid function
+			}
+			animator(time);
+		}
+	}
+	
+	def linearAnimator(multiplier:Double, time:Double):Unit = {
+		val c = toDouble(current);
+		val t = toDouble(target);
+		val d = time * multiplier;
+		var n = c;
+		if (c < t) {
+			n += d;
+			if (n > t) {
+				n = t;
+			}
+		} else if (c > t) {
+			n -= d;
+			if (n < t) {
+				n = t;
+			}
+		}
+	}
 }
--- src/main/scala/com/sgine/work/unit/RepeatingUnit.scala
+++ src/main/scala/com/sgine/work/unit/RepeatingUnit.scala
@@ -1,15 +1,25 @@
 package com.sgine.work.unit
 
+import java.lang.ref._;
+
 trait RepeatingUnit extends WorkUnit {
-	var unit:Function0[Unit];
+	private var unitRef:WeakReference[Function0[Unit]] = _;
+	
+	def unit = unitRef.get();
+	
+	def unit_=(_unit:Function0[Unit]) = {
+		unitRef = new WeakReference[Function0[Unit]](_unit);
+	}
 	
 	abstract override def apply():Unit = {
 		super.apply();
 		
-		if (unit != null) {
+		if (unitRef != null) {
+			if (unit == null) {
+				return;					// Unit has lost its reference, so stop repeating
+			}
 			unit();
-			
-			workManager += this;			// Re-queue
 		}
+		workManager += this;			// Re-queue
 	}
 }
