9aebbe31e0b5afb0c061689efffb7f9a39a98c43
darkfrog26 <darkfrog26@localhost>
2009-11-10 20:17:01 +0000

    Updates to property tests

--- src/main/scala/com/sgine/property/TestProperties.scala
+++ src/main/scala/com/sgine/property/TestProperties.scala
@@ -1,13 +1,28 @@
 package com.sgine.property
 
+import com.sgine.work._;
+import com.sgine.work.unit._;
+
 object TestProperties {
 	def main(args:Array[String]) = {
 		val p = new MutableProperty[Int] with Testing[Int];
 		p(5);
 		p(10);
 		println("Value: " + p());
+		
+		val ut = new UpdateTest();
+		ut.unit = test;
+		DefaultWorkManager += ut;
+		
+		Thread.sleep(4000);
+	}
+	
+	def test() = {
+		println("Repeating!");
 	}
 }
+
+class UpdateTest extends RepeatingUnit;
 
 trait Testing[T] extends Changeable[T] {
 	abstract override def changed(oldValue:T, newValue:T) = {
