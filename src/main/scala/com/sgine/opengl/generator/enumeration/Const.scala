598a82a30fd1f4cc0c603b93e66f23751f3f7812
Hans.Haggstrom <Hans.Haggstrom@localhost>
2009-11-12 20:55:49 +0000

    Removed unused and unimplemented openGL docbook parser.

--- src/main/scala/com/sgine/opengl/generator/enumeration/Const.scala
+++
@@ -1,6 +0,0 @@
-package com.sgine.opengl.generator.enumeration
-
-/**
- * An enumeration constant that is a member of some Enum.
- */
-case class Const( name : String, value : String ) 
--- src/main/scala/com/sgine/opengl/generator/enumeration/Enum.scala
+++
@@ -1,6 +0,0 @@
-package com.sgine.opengl.generator.enumeration
-
-/**
- * Represents an enumeration in OpenGL, as parsed from http://www.opengl.org/registry/api/enum.spec
- */
-case class Enum(name : String, values : List[Const] )
