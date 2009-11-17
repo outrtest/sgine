73fddae59f5a94f89592384e83f8e3b346a139af
Hans.Haggstrom <Hans.Haggstrom@localhost>
2009-11-17 23:13:05 +0000

    Whitespace...

--- src/main/scala/com/sgine/math/packageobject.scala
+++ src/main/scala/com/sgine/math/packageobject.scala
@@ -2,5 +2,5 @@ import com.sgine.math.Vector3
 
 package object math {
 	implicit def tuple3dToVector3(value:(Double, Double, Double)) : Vector3 = new Vector3(value._1, value._2, value._3);
-	implicit def tuple3iToVector3(value:(Int, Int, Int)) : Vector3 = new Vector3(value._1, value._2, value._3);
+	implicit def tuple3iToVector3(value:(Int,    Int,    Int   )) : Vector3 = new Vector3(value._1, value._2, value._3);
 }
