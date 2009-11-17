02e6d8d2ef7428c9370a3913848ee0abbb8d349d
Hans.Haggstrom <Hans.Haggstrom@localhost>
2009-11-17 23:11:48 +0000

    Fixed implicit conversion method naming.

--- src/main/scala/com/sgine/math/packageobject.scala
+++ src/main/scala/com/sgine/math/packageobject.scala
@@ -1,6 +1,6 @@
 import com.sgine.math.Vector3
 
 package object math {
-	implicit def tuple3dToVector3d(value:(Double, Double, Double)) : Vector3 = Vector3(value._1, value._2, value._3);
-	implicit def tuple3iToVector3d(value:(Int, Int, Int)) : Vector3 = Vector3(value._1, value._2, value._3);
+	implicit def tuple3dToVector3(value:(Double, Double, Double)) : Vector3 = new Vector3(value._1, value._2, value._3);
+	implicit def tuple3iToVector3(value:(Int, Int, Int)) : Vector3 = new Vector3(value._1, value._2, value._3);
 }
