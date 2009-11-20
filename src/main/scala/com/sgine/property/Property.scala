0d45676d99342942a9fe2950d7951704eeef27e6
darkfrog26 <darkfrog26@localhost>
2009-11-20 19:34:11 +0000

    Added implicit conversion from Property[T] to T and created ":=" method as an alias to apply(value) in Property

--- src/main/scala/com/sgine/property/Property.scala
+++ src/main/scala/com/sgine/property/Property.scala
@@ -1,3 +1,7 @@
 package com.sgine.property;
 
-trait Property[T] extends (() => T) with (T => Property[T])
+trait Property[T] extends (() => T) with (T => Property[T]) {
+	def :=(value:T):Property[T] = {
+		apply(value);
+	}
+}
