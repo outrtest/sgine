8a270958200cadf4712212961ac071efb0c373ed
darkfrog26 <darkfrog26@localhost>
2009-11-29 21:00:51 +0000

    Added SGINE logo EPS file Finished configuring ScalaTest to work with Ant

--- src/test/scala/com/sgine/property/PropertyTests.scala
+++ src/test/scala/com/sgine/property/PropertyTests.scala
@@ -3,7 +3,7 @@ package com.sgine.property
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
-class ProptertyTests extends FlatSpec with ShouldMatchers {
+class PropertyTests extends FlatSpec with ShouldMatchers {
 	"Property" should "assign values properly" in {
 		val p = new ImmutableProperty(5)
 		p() should equal (5)
