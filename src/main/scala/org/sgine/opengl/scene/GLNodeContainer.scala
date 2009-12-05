dc877848583972b669a9b3f96a1057b307aa8aed
darkfrog26 <darkfrog26@localhost>
2009-12-05 16:51:20 +0000

    Created GLNodeContainer to contain matrix location information for offsetting Created Mesh trait for "visual" package for later implementation

---
+++ src/main/scala/org/sgine/opengl/scene/GLNodeContainer.scala
@@ -0,0 +1,10 @@
+package org.sgine.opengl.scene
+
+import org.sgine.scene._
+
+/**
+ * GLNodeContainer simply adds a matrix location to determine offset for child nodes
+ */
+class GLNodeContainer extends GeneralNodeContainer {
+
+}
---
+++ src/main/scala/org/sgine/visual/Mesh.scala
@@ -0,0 +1,5 @@
+package org.sgine.visual
+
+trait Mesh {
+	// TODO: should contain vertices, faces indices, and should only use triangles
+}
