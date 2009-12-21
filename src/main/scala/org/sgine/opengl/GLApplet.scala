08b869f60bfbb6b5253f0f7e34deec3ae52b21dc
darkfrog26 <darkfrog26@localhost>
2009-12-21 01:48:40 +0000

    Continued development of applet support Created basic java.awt.Desktop wrapper that also takes command-line arguments

--- src/main/scala/org/sgine/opengl/GLApplet.scala
+++ src/main/scala/org/sgine/opengl/GLApplet.scala
@@ -3,6 +3,12 @@ package org.sgine.opengl
 class GLApplet extends java.applet.Applet with GLContainer {
 	val awtContainer = this
 	
+	override final def init = {
+		super.init()
+		
+		begin()
+	}
+	
 	override def begin() = {
 		containerWidth := getWidth
 		containerHeight := getHeight
