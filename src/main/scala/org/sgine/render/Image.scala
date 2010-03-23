7cb7baec10e259a7ed200d96f4d9cb0e6d4bc964
Matt Hicks <mhicks@sgine.org>
2010-03-22 19:43:36 -0500

    Removed existing work on org.sgine.render to begin working anew

--- src/main/scala/org/sgine/render/Image.scala
+++
@@ -1,7 +0,0 @@
-package org.sgine.render
-
-case class Image(texture: Texture, x: Float, y: Float, width: Float, height: Float) {
-	def draw(x: Float, y: Float, width: Float, height: Float) = {
-//		texture.bi
-	}
-}
