61eacce2251fb161cc1caf6ab0792cfcf1f17c81
Matt Hicks <mhicks@sgine.org>
2010-03-30 20:57:26 -0500

    Modified Image to use Double instead of Float Changed RenderList to take an Iterable instead of List Added a convenience method to TextureUtil to accept a URL Created TextureMap to map Images to a Texture by name Created AngelCodeFont and AngelCodeFontChar to represent AngelCode bitmapped fonts

--- src/main/scala/org/sgine/render/Image.scala
+++ src/main/scala/org/sgine/render/Image.scala
@@ -2,16 +2,16 @@ package org.sgine.render
 
 import org.lwjgl.opengl.GL11._
 
-class Image private() extends Function0[Unit] {
+class Image protected() extends Function0[Unit] {
 	var texture: Texture = _
-	var x: Float = _
-	var y: Float = _
-	var width: Float = _
-	var height: Float = _
+	var x: Double = _
+	var y: Double = _
+	var width: Double = _
+	var height: Double = _
 	
 	def apply() = draw()
 	
-	def draw(offsetX: Float = 0.0f, offsetY: Float = 0.0f, insetX1: Float = 0.0f, insetY1: Float = 0.0f, insetX2: Float = 0.0f, insetY2: Float = 0.0f) = {
+	def draw(offsetX: Double = 0.0, offsetY: Double = 0.0, insetX1: Double = 0.0, insetY1: Double = 0.0, insetX2: Double = 0.0, insetY2: Double = 0.0) = {
 		if (texture != null) {
 			texture.bind()
 		}
@@ -20,26 +20,26 @@ class Image private() extends Function0[Unit] {
 		val y1 = (y + insetY1) / texture.height
 		val x2 = (x + width - insetX2) / texture.width
 		val y2 = (y + height - insetY2) / texture.height
-		val w = (width - insetX1 - insetX2) / 2.0f
-		val h = (height - insetY1 - insetY2) / 2.0f
+		val w = (width - insetX1 - insetX2) / 2.0
+		val h = (height - insetY1 - insetY2) / 2.0
 		
 		glBegin(GL_QUADS)
 		
 		// Bottom-Left
-		glTexCoord2f(x1, y2)
-		glVertex3f(-w + offsetX, -h + offsetY, 0.0f)
+		glTexCoord2d(x1, y2)
+		glVertex3d(-w + offsetX, -h + offsetY, 0.0)
 		
 		// Bottom-Right
-		glTexCoord2f(x2, y2)
-		glVertex3f(w + offsetX, -h + offsetY, 0.0f)
+		glTexCoord2d(x2, y2)
+		glVertex3d(w + offsetX, -h + offsetY, 0.0)
 		
 		// Top-Right
-		glTexCoord2f(x2, y1)
-		glVertex3f(w + offsetX, h + offsetY, 0.0f)
+		glTexCoord2d(x2, y1)
+		glVertex3d(w + offsetX, h + offsetY, 0.0)
 		
 		// Top-Left
-		glTexCoord2f(x1, y1)
-		glVertex3f(-w + offsetX, h + offsetY, 0.0f)
+		glTexCoord2d(x1, y1)
+		glVertex3d(-w + offsetX, h + offsetY, 0.0)
 		
 		glEnd()
 	}
@@ -53,6 +53,17 @@ object Image {
 		i.texture = texture
 		i.width = texture.width
 		i.height = texture.height
+		
+		i
+	}
+	
+	def apply(texture: Texture, x: Double, y: Double, width: Double, height: Double) = {
+		val i = new Image()
+		i.texture = texture
+		i.x = x
+		i.y = y
+		i.width = width
+		i.height = height
 		
 		i
 	}
