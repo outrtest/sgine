c2469e5cbbcb3d7e645aa4f7de1a95a040c33b40
Matt Hicks <mhicks@sgine.org>
2010-04-22 19:39:08 -0500

    Updated to LWJGL 2.4.2 Modified Mouse to be updated by Renderer to maintain context for events Updated Ray to properly transform Created Renderer.translateLocal to get a point at a specific coordinate applied via a specific Matrix4 Modified Renderer to use glFrustum instead gluPerspective Created TestMousePicking

--- src/main/scala/org/sgine/input/Mouse.scala
+++ src/main/scala/org/sgine/input/Mouse.scala
@@ -5,18 +5,20 @@ import org.sgine.event.Listenable
 
 import org.sgine.input.event.MouseEvent
 
+import org.sgine.render.Renderer
+
 import org.sgine.work.Updatable
 
 import org.lwjgl.input.{Mouse => GLMouse}
 
-object Mouse extends Listenable with Updatable {
+object Mouse extends Listenable {
 	def validate() = {
 		if (!GLMouse.isCreated) {
 			GLMouse.create()
 		}
 	}
 	
-	def update(time: Double) = {
+	def update(renderer: Renderer) = {
 		if (GLMouse.isCreated) {
 			while (GLMouse.next()) {
 				val button = GLMouse.getEventButton
@@ -27,9 +29,18 @@ object Mouse extends Listenable with Updatable {
 				val dx = GLMouse.getEventDX
 				val dy = GLMouse.getEventDY
 				
-				// TODO: update to context for mouse
+				val windowX = x / (renderer.canvas.getWidth / 2.0) - 1.0
+				val windowY = y / (renderer.canvas.getHeight / 2.0) - 1.0
+				val windowDX = dx / (renderer.canvas.getWidth / 2.0)
+				val windowDY = dy / (renderer.canvas.getHeight / 2.0)
+
+				val nearHeight = 1.0
+				val normalizedX = nearHeight * (renderer.canvas.getWidth.asInstanceOf[Double] / renderer.canvas.getHeight.asInstanceOf[Double]) * windowX
+				val normalizedY = nearHeight * windowY
+				val normalizedDX = nearHeight * (renderer.canvas.getWidth.asInstanceOf[Double] / renderer.canvas.getHeight.asInstanceOf[Double]) * windowDX
+				val normalizedDY = nearHeight * windowDY
 				
-				val evt = MouseEvent(button, state, wheel, x, y, dx, dy)
+				val evt = MouseEvent(button, state, wheel, normalizedX, normalizedY, normalizedDX, normalizedDY)
 				Event.enqueue(evt)
 			}
 		}
--- src/test/scala/org/sgine/ui/TestCube.scala
+++ src/test/scala/org/sgine/ui/TestCube.scala
@@ -21,63 +21,49 @@ import org.sgine.scene.GeneralNodeContainer
 object TestCube {
 	def main(args: Array[String]): Unit = {
 		val r = Renderer.createFrame(1024, 768, "Test Cube", 4, 8, 4, 4)
-		r.verticalSync := true
+		r.verticalSync := false
 		
 		val scene = new GeneralNodeContainer()
 		
 		val cube = new ImageCube()
 		cube.location.z := -1500.0
 		cube.rotation.x.adjuster = new EasingNumericAdjuster(Linear.easeIn, 2.0)
-//		cube.rotation.y.adjuster = new EasingNumericAdjuster(Linear.easeIn, 4.0)
-//		cube.rotation.z.adjuster = new EasingNumericAdjuster(Linear.easeIn, 6.0)
-//		cube.location.x.adjuster = new EasingNumericAdjuster(Elastic.easeInOut, 4.0)
+		cube.rotation.y.adjuster = new EasingNumericAdjuster(Linear.easeIn, 4.0)
+		cube.rotation.z.adjuster = new EasingNumericAdjuster(Linear.easeIn, 6.0)
+		cube.location.x.adjuster = new EasingNumericAdjuster(Elastic.easeInOut, 4.0)
 		cube(Resource("resource/sgine_256.png"), 256.0, 256.0)
 		scene += cube
 		
-		r.renderable := RenderableScene(scene, showFPS = false)
+		r.renderable := RenderableScene(scene, showFPS = true)
 		
 		// Rotate the cube perpetually on the x-axis
 		val rx1 = new PropertyChangeEffect(cube.rotation.x, Math.Pi * 2.0)
-//		val rx2 = new PropertySetEffect(cube.rotation.x, 0.0)
-//		val rotateX = new CompositeEffect(rx1, rx2)
-//		rotateX.repeat = -1
+		val rx2 = new PropertySetEffect(cube.rotation.x, 0.0)
+		val rotateX = new CompositeEffect(rx1, rx2)
+		rotateX.repeat = -1
 		
-//		// Rotate the cube perpetually on the y-axis
-//		val ry1 = new PropertyChangeEffect(cube.rotation.y, Math.Pi * 2.0)
-//		val ry2 = new PropertySetEffect(cube.rotation.y, 0.0)
-//		val rotateY = new CompositeEffect(ry1, ry2)
-//		rotateY.repeat = -1
-//		
-//		// Rotate the cube perpetually on the z-axis
-//		val rz1 = new PropertyChangeEffect(cube.rotation.z, Math.Pi * 2.0)
-//		val rz2 = new PropertySetEffect(cube.rotation.z, 0.0)
-//		val rotateZ = new CompositeEffect(rz1, rz2)
-//		rotateZ.repeat = -1
-//		
-//		// Move the cube back and forth perpetually on the x-axis
-//		val me1 = new PropertyChangeEffect(cube.location.x, -600.0)
-//		val me2 = new PropertyChangeEffect(cube.location.x, 600.0)
-//		val move = new CompositeEffect(me1, me2)
-//		move.repeat = -1
+		// Rotate the cube perpetually on the y-axis
+		val ry1 = new PropertyChangeEffect(cube.rotation.y, Math.Pi * 2.0)
+		val ry2 = new PropertySetEffect(cube.rotation.y, 0.0)
+		val rotateY = new CompositeEffect(ry1, ry2)
+		rotateY.repeat = -1
+		
+		// Rotate the cube perpetually on the z-axis
+		val rz1 = new PropertyChangeEffect(cube.rotation.z, Math.Pi * 2.0)
+		val rz2 = new PropertySetEffect(cube.rotation.z, 0.0)
+		val rotateZ = new CompositeEffect(rz1, rz2)
+		rotateZ.repeat = -1
+		
+		// Move the cube back and forth perpetually on the x-axis
+		val me1 = new PropertyChangeEffect(cube.location.x, -600.0)
+		val me2 = new PropertyChangeEffect(cube.location.x, 600.0)
+		val move = new CompositeEffect(me1, me2)
+		move.repeat = -1
 		
 		// Start effects
-//		rotateX.start()
-//		rotateY.start()
-//		rotateZ.start()
-//		move.start()
-		
-		while (true) {
-			if (!rx1.playing) {
-				println("Pressing play!")
-				cube.rotation.x.set(0.0)
-				rx1.start()
-			}
-			
-			Thread.sleep(1)
-			
-//			cube.rotation.x := Math.Pi * 2.0
-//			cube.rotation.x.waitForTarget()
-//			cube.rotation.x.set(0.0)
-		}
+		rotateX.start()
+		rotateY.start()
+		rotateZ.start()
+		move.start()
 	}
 }
