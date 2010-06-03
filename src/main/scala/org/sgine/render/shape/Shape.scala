702b955ad6ca44608a064c1e9306b9c4ee231666
Matt Hicks <mhicks@sgine.org>
2010-06-03 09:26:24 -0500

    Fixed invalid hex code in Color.Blue Updated TestRenderer to display correctly Created render.shape.Shape for shape drawing Created render.shape.Quad for basic quad support

---
+++ src/main/scala/org/sgine/render/shape/Shape.scala
@@ -0,0 +1,38 @@
+package org.sgine.render.shape
+
+import org.lwjgl.opengl.GL11._
+
+trait Shape extends Function0[Unit] {
+	def mode: Int
+	def vertexCount: Int
+	def color(index: Int): Unit
+	def texture(index: Int): Unit
+	def vertex(index: Int): Unit
+	
+	private var range: Range = _
+	
+	def apply() = {
+		if ((range == null) || (range.length != vertexCount)) {
+			range = 0 until vertexCount
+		}
+		
+		
+		begin()
+		range.foreach(renderVertex)
+		end()
+	}
+	
+	protected def begin() = {
+		glBegin(mode)
+	}
+	
+	protected def end() = {
+		glEnd()
+	}
+	
+	private val renderVertex = (index: Int) => {
+		color(index)
+		texture(index)
+		vertex(index)
+	}
+}
