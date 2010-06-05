95b5503d58c80d9c73575ca352c2fcf86c7a190c
Matt Hicks <mhicks@sgine.org>
2010-06-04 20:10:27 -0500

    Removed dependencies to zShapes

--- src/test/scala/org/sgine/ui/TestGraphics3D.scala
+++ src/test/scala/org/sgine/ui/TestGraphics3D.scala
@@ -17,9 +17,6 @@ import org.lwjgl.opengl.GL11._
 
 import scala.math._
 
-import gr.zdimensions.zshapes.viewer.font._
-import gr.zdimensions.zshapes.viewer.renderer.ShapeRenderer
-
 object TestGraphics3D {
 	def main(args: Array[String]): Unit = {
 		val r = Renderer.createFrame(1024, 768, "Test RenderScene", 4, 8, 4, 4)
@@ -35,18 +32,9 @@ object TestGraphics3D {
 }
 
 class TestGraphics3D extends AdvancedComponent {
-	private lazy val caps = GLContext.getCapabilities()
-	private lazy val r = ShapeRenderer.shapeRendererGetAA(caps)
-	private lazy val fontMap = new FontMap("Z:/programming/classpath/zShapes/samples/test.zfm")
-	private lazy val font = new Font(fontMap, 80.0f)
-	private lazy val renderable = new TextArea(font, "Hello World!")
-	lineWidth = 2.0
-	
 	def drawComponent() = {
 		drawRect(0.0, 0.0, 100.0, 100.0)
 		
 		drawRoundRect(0.0, 115.0, 100.0, 100.0, 10)
-		
-		r.render(renderable)
 	}
 }
