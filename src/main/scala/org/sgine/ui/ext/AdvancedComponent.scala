50eec4c33e12e6c6119c837ec79c3ef20476ce5a
Matt Hicks <mhicks@sgine.org>
2010-07-03 07:37:08 -0500

    Switched mix-in order for AdvancedComponent traits

--- src/main/scala/org/sgine/ui/ext/AdvancedComponent.scala
+++ src/main/scala/org/sgine/ui/ext/AdvancedComponent.scala
@@ -2,4 +2,4 @@ package org.sgine.ui.ext
 
 import org.sgine.ui.Component
 
-trait AdvancedComponent extends Component with LocationComponent with RotationComponent with ScaleComponent
+trait AdvancedComponent extends Component with ScaleComponent with RotationComponent with LocationComponent
