30a84ff01dc569a0fe4993343ef684a287d1d78e
Matt Hicks <mhicks@sgine.org>
2010-07-03 07:23:29 -0500

    Updates to resolve issues with simplex conversion in "ui" package

--- src/main/scala/org/sgine/scene/ext/ResolutionNode.scala
+++ src/main/scala/org/sgine/scene/ext/ResolutionNode.scala
@@ -5,6 +5,6 @@ import simplex3d.math.doublem._
 
 trait ResolutionNode extends WorldMatrixNode {
 	def setResolution(width: Double, height: Double) = {
-		worldMatrix := Mat3x4d.Identity.translate(Vec3d(0.0, 0.0, -101.0)).scale(2.02 / height)
+		worldMatrix := Mat3x4d.Identity.scale(2.02 / height).translate(Vec3d(0.0, 0.0, -101.0))
 	}
 }
