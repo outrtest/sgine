1bbe7ef05c60c86d5b1ea119551136970fdf82c3
Matt Hicks <mhicks@sgine.org>
2010-07-04 07:48:47 -0500

    Added "maxTime" to Renderer and FPSLabel to show delays created between renders.

--- src/main/scala/org/sgine/render/Debug.scala
+++ src/main/scala/org/sgine/render/Debug.scala
@@ -19,7 +19,7 @@ trait Debug extends Display {
 		scene += debugContainer
 		
 		val fps = new FPSLabel()
-		fps.font := FontManager("lcd")
+		fps.font := FontManager("Arial32")
 		fps.location.x := -795.0
 		fps.location.x.align := org.sgine.core.HorizontalAlignment.Left
 		fps.location.y := 595.0
