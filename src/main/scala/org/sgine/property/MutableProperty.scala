fcffbea0d36e6bc8a969184b05c97941d711ebb6
Matt Hicks <mhicks@sgine.org>
2010-07-04 08:05:20 -0500

    Further hacking to MutableProperty to force a change to be thrown for Mat3x4d

--- src/main/scala/org/sgine/property/MutableProperty.scala
+++ src/main/scala/org/sgine/property/MutableProperty.scala
@@ -24,6 +24,9 @@ class MutableProperty[T] extends Property[T] {
 		import simplex3d.math.doublem._
 		if (this.value.isInstanceOf[Mat3x4d]) {
 			this.value.asInstanceOf[Mat3x4d] := value.asInstanceOf[Mat3x4d]
+			if (this.isInstanceOf[ChangeableProperty[_]]) {
+				this.asInstanceOf[ChangeableProperty[T]].changed(this.value, this.value)
+			}
 		}
 		else
 			this.value = value;
