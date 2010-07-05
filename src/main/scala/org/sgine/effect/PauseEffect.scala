abc768c09b501c597604155355e399ca039bc578
Matt Hicks <mhicks@sgine.org>
2010-07-05 17:32:06 -0500

    Fixed bug in PauseEffect Updated TestCube to be more *random*

--- src/main/scala/org/sgine/effect/PauseEffect.scala
+++ src/main/scala/org/sgine/effect/PauseEffect.scala
@@ -5,22 +5,26 @@ import org.sgine.work.Updatable
 class PauseEffect(duration: Double) extends Effect with Updatable {
 	private var running: Boolean = false
 	private var elapsed: Double = 0.0
+	private var isFinished: Boolean = false
 	
 	def play() = {
 		initUpdatable()
 		
 		elapsed = 0.0
 		running = true
+		isFinished = false
 	}
 	
-	def finished = running
+	def finished = isFinished
 	
 	override def update(time: Double) = {
 		if (running) {
 			elapsed += time
 			
 			if (elapsed >= duration) {
+				elapsed = 0.0
 				running = false
+				isFinished = true
 			}
 		}
 		
