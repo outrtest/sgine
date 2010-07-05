a1f425933b3747e4655efeeff431f0032a769dc1
Matt Hicks <mhicks@sgine.org>
2010-07-05 17:15:34 -0500

    Converted TestCube to be a performance test

--- src/test/scala/org/sgine/ui/TestCube.scala
+++ src/test/scala/org/sgine/ui/TestCube.scala
@@ -19,23 +19,39 @@ import org.sgine.render.Renderer
 import org.sgine.render.StandardDisplay
 import org.sgine.render.scene.RenderableScene
 
-import org.sgine.scene.GeneralNodeContainer
+import org.sgine.scene.MutableNodeContainer
 
 import scala.math._
 
 object TestCube extends StandardDisplay with Debug {
 	def setup() = {
+		for (row <- 0 until 10) {
+			for (column <- 0 until 10) {
+				CubeCreator.createCube(scene, column, row)
+				Thread.sleep(200)
+			}
+		}
+	}
+	
+	private def test(evt: org.sgine.event.Event) = {
+		println(evt)
+	}
+}
+
+object CubeCreator {
+	def createCube(scene: MutableNodeContainer, y: Double, z: Double) = {
 		val cube = new ImageCube()
 		val c = MutableColor(Color.White)
 		cube.color := c
 //		cube.alpha := 0.5
-		cube.scale.set(0.5)
+		cube.scale.set(0.1)
+		cube.location.y := (y * 60.0) - 350.0
+		cube.location.z := (z * 100.0) - 800.0
 		cube.rotation.x.animator = new LinearNumericAnimator(2.0)
 		cube.rotation.y.animator = new LinearNumericAnimator(2.0)
 		cube.rotation.z.animator = new LinearNumericAnimator(2.0)
 		cube.location.x.animator = new EasingNumericAnimator(Elastic.easeInOut, 3.0)
 		cube(Resource("sgine_256.png"), 256.0, 256.0)
-		cube.front().listeners += test _
 		scene += cube
 		
 		cube.rotation.x := Double.MaxValue
@@ -48,9 +64,5 @@ object TestCube extends StandardDisplay with Debug {
 		val move = new CompositeEffect(me1, me2)
 		move.repeat = -1
 		move.start()
-	}
-	
-	private def test(evt: org.sgine.event.Event) = {
-		println(evt)
 	}
 }
