fa91d5a0e273b2630e3ccd4574f20b8bc69a67d0
Matt Hicks <mhicks@sgine.org>
2010-07-06 18:04:30 -0500

    Added scale-9 images for TextField on Windows 7 style

--- src/test/scala/org/sgine/ui/TestMediaPlayer.scala
+++ src/test/scala/org/sgine/ui/TestMediaPlayer.scala
@@ -7,6 +7,8 @@ import org.sgine.render.StandardDisplay
 
 object TestMediaPlayer extends StandardDisplay with Debug {
 	def setup() = {
+		renderer.verticalSync := true
+		
 		val component = new MediaPlayer()
 		val url = new java.net.URL("http://sun.edgeboss.net/download/sun/media/1460825906/1460825906_2956241001_big-buck-bunny-640x360.flv")
 		component.source := Resource(url)
