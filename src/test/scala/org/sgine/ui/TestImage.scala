76b2d5b0e2365b0763d2acb4f930518e54a34ead
Matt Hicks <mhicks@sgine.org>
2010-07-11 15:47:24 -0500

    Reverted test changes to TestImage

--- src/test/scala/org/sgine/ui/TestImage.scala
+++ src/test/scala/org/sgine/ui/TestImage.scala
@@ -12,11 +12,6 @@ import org.sgine.render.StandardDisplay
  */
 object TestImage extends StandardDisplay with Debug {
 	def setup() = {
-		renderer.invokeLater(test _)
-		renderer.light0.enabled := true
-		renderer.light0.ambience := org.sgine.core.Color(0.2, 0.3, 0.6, 1.0)
-		renderer.light0.diffuse := org.sgine.core.Color(0.2, 0.3, 0.6, 1.0)
-		
 		val component = new Image(Resource("puppies.jpg"))
 		scene += component
 	}
