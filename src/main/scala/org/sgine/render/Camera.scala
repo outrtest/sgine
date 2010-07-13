f2ff046d35b37ca5e3682d72558d28ed2c0b91fd
lexn82 <lexn82@localhost>
2010-07-13 15:17:56 -0400

    Added camera to the renderer.

--- src/main/scala/org/sgine/render/Camera.scala
+++ src/main/scala/org/sgine/render/Camera.scala
@@ -1,20 +1,46 @@
+// This file is under BSD license.
+
 package org.sgine.render
 
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
+import simplex3d.math.doublem.renamed._
+import simplex3d.math.doublem.DoubleMath._
 
-import simplex3d.math._
-import simplex3d.math.doublem._
+/**
+ *
+ * @author Aleksey Nikiforov (lex)
+ */
+class Camera(
+  val view: Mat3x4 = inverseTransformation(Vec3.One, Mat3.Identity, Vec3.Zero),
+  val projection: Mat4 = orthoProj(-100, 100, -100, 100, 5, 1000)
+) {
+  private val viewProjection = Mat4(0)
+  private val inverseViewProjection = Mat4(0)
 
-class Camera {
-	var matrix = Mat3x4d.Identity
-	
-	val buffer = ByteBuffer.allocateDirect(128).order(ByteOrder.nativeOrder).asDoubleBuffer
-	
-	validate()
-	
-	def validate() = {
-		matrixToBuffer(matrix, buffer)
-		buffer.flip()
-	}
+  // TODO: Change to use properties and auto-update
+  def update() {
+    viewProjection := projection*Mat4(view)
+    inverseViewProjection := inverse(viewProjection)
+  }
+
+  def screenToWorldCoords(screenCoords: inVec3, screenDimensions: inVec2) = {
+    val normalizedScreenCoords = Vec4(
+      2*screenCoords.x/screenDimensions.x - 1,
+      2*screenCoords.y/screenDimensions.y - 1,
+      screenCoords.z,
+      1
+    )
+    val worldCoords = inverseViewProjection*normalizedScreenCoords
+    worldCoords.xyz/worldCoords.w
+  }
+
+  def worldToScreenCoords(worldCoords: inVec3, screenDimensions: inVec2) = {
+    val halfScreen = screenDimensions/2
+    val transformed = viewProjection*Vec4(worldCoords, 1)
+    val normalizedScreenCoords = transformed.xyz/transformed.w
+    Vec3(
+      normalizedScreenCoords.x*halfScreen.x + halfScreen.x,
+      normalizedScreenCoords.y*halfScreen.y + halfScreen.y,
+      normalizedScreenCoords.z
+    )
+  }
 }
