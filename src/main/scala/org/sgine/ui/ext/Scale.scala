6fb2d06b0475cb0345b8b3fac67c22c34fb9dd1a
Matt Hicks <mhicks@sgine.org>
2010-07-18 14:57:57 -0500

    Fixed Scale to use NumericProperty instead of AdvancedProperty

--- src/main/scala/org/sgine/ui/ext/Scale.scala
+++ src/main/scala/org/sgine/ui/ext/Scale.scala
@@ -1,14 +1,14 @@
 package org.sgine.ui.ext
 
-import org.sgine.property.AdvancedProperty
+import org.sgine.property.NumericProperty
 import org.sgine.property.container.PropertyContainer
 
 import org.sgine.ui.Component
 
 class Scale(override val parent: Component) extends PropertyContainer {
-	val x = new AdvancedProperty(1.0, this)
-	val y = new AdvancedProperty(1.0, this)
-	val z = new AdvancedProperty(1.0, this)
+	val x = new NumericProperty(1.0, this)
+	val y = new NumericProperty(1.0, this)
+	val z = new NumericProperty(1.0, this)
 	
 	def set(s: Double) = {
 		x := s
