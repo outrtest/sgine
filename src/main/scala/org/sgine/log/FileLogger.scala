ebefcc46e51633025bda440ecd21d5856d856ea6
Matt Hicks <mhicks@sgine.org>
2010-07-28 15:48:55 -0500

    Created FileLogger Created LogSpec to unit test logging functionality

--- src/main/scala/org/sgine/log/FileLogger.scala
+++ src/main/scala/org/sgine/log/FileLogger.scala
@@ -1,9 +1,14 @@
 package org.sgine.log
 
+import java.io.BufferedWriter
 import java.io.File
+import java.io.FileWriter
 
 class FileLogger(file: File) extends Logger {
+	private val out = new BufferedWriter(new FileWriter(file))
+	
 	def log(log: Log) = {
-		// TODO: implement
+		out.write(log.toString)
+		out.newLine()
 	}
 }
