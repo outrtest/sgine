0bc7ed9828a9b7a4755ea38cada8eb8196e8cf60
Matt Hicks <mhicks@sgine.org>
2010-07-29 20:25:25 -0500

    Added features to Debug trait: Escape exits application and "L" toggles lighting Created global "lighting" property in Renderer Added bounding support to ui.Box

--- src/main/scala/org/sgine/render/Debug.scala
+++ src/main/scala/org/sgine/render/Debug.scala
@@ -1,5 +1,12 @@
 package org.sgine.render
 
+import org.sgine.event.EventHandler
+import org.sgine.event.ProcessingMode
+
+import org.sgine.input.Key
+import org.sgine.input.Keyboard
+import org.sgine.input.event.KeyPressEvent
+
 import org.sgine.render.font.FontManager
 
 import org.sgine.scene.GeneralNodeContainer
@@ -26,5 +33,16 @@ trait Debug extends Display {
 		fps.location.y.align := org.sgine.core.VerticalAlignment.Top
 		fps.location.z := 1.0
 		debugContainer += fps
+		
+		Keyboard.listeners += EventHandler(handleKey, ProcessingMode.Blocking)
+	}
+	
+	private def handleKey(evt: KeyPressEvent) = {
+		if (evt.key == Key.Escape) {					// Shutdown
+			Renderer().shutdown()
+		} else if (evt.keyChar.toLower == 'l') {		// Toggle lighting
+			Renderer().lighting := !Renderer().lighting()
+			println("Lighting turned " + (if (Renderer().lighting()) "on" else "off"))
+		}
 	}
 }
