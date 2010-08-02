03fcc6196adff567921df5eeffdc7afdca808923
Matt Hicks <mhicks@sgine.org>
2010-08-02 13:33:27 -0500

    Modified default DB to be DB4O as Neodatis has problems Added DB.query(Class) method Added DB.find(Class) convenience method Updated db.db4o.DB to support DBFactory Updated DBSpec to test full range of expected functionality

--- src/main/scala/org/sgine/db/db4o/RichObjectSet.scala
+++
@@ -1,8 +0,0 @@
-package org.sgine.db.db4o
-
-import com.db4o.ObjectSet
-
-class RichObjectSet[T](objectSet: ObjectSet[T]) extends Iterator[T] {
-	def hasNext = objectSet.hasNext
-	def next = objectSet.next
-}
