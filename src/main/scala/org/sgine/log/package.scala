b34e5166003003c4d0e4d748dac2c2639aef8a28
Matt Hicks <mhicks@sgine.org>
2010-08-31 12:33:36 -0500

    Updated Log and WebLog to reverse order method, className to className, method and added lineNumber. Added support for Log.sourceLookup to allow automatic determination of className, method, and lineNumber for the call if enabled. Defaults to false.

--- src/main/scala/org/sgine/log/package.scala
+++ src/main/scala/org/sgine/log/package.scala
@@ -5,69 +5,77 @@ package object log {
 	
 	def finest(message: Any,
 			 messageType: String = null,
-			 method: String = null,
 			 className: String = null,
+			 method: String = null,
+			 lineNumber: String = null,
 			 level: LogLevel = LogLevel.Finest,
 			 reference: AnyRef = null,
-			 args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			 args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			 
 	def finer(message: Any,
 			 messageType: String = null,
-			 method: String = null,
 			 className: String = null,
+			 method: String = null,
+			 lineNumber: String = null,
 			 level: LogLevel = LogLevel.Finer,
 			 reference: AnyRef = null,
-			 args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			 args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			 
 	def fine(message: Any,
 			 messageType: String = null,
-			 method: String = null,
 			 className: String = null,
+			 method: String = null,
+			 lineNumber: String = null,
 			 level: LogLevel = LogLevel.Fine,
 			 reference: AnyRef = null,
-			 args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			 args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			 
 	def debug(message: Any,
 			  messageType: String = null,
-			  method: String = null,
 			  className: String = null,
+			  method: String = null,
+			  lineNumber: String = null,
 			  level: LogLevel = LogLevel.Debug,
 			  reference: AnyRef = null,
-			  args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			  args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			  
 	def config(message: Any,
 			  messageType: String = null,
-			  method: String = null,
 			  className: String = null,
+			  method: String = null,
+			  lineNumber: String = null,
 			  level: LogLevel = LogLevel.Config,
 			  reference: AnyRef = null,
-			  args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			  args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 	
 	val info = Log
 	
 	def warn(message: Any,
 			 messageType: String = null,
-			 method: String = null,
 			 className: String = null,
+			 method: String = null,
+			 lineNumber: String = null,
 			 level: LogLevel = LogLevel.Warning,
 			 reference: AnyRef = null,
-			 args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			 args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			 
 	def trace(message: Any,
 			  throwable: Throwable,
 			  messageType: String = "stacktrace",
-			  method: String = null,
 			  className: String = null,
+			  method: String = null,
+			  lineNumber: String = null,
 			  level: LogLevel = LogLevel.Warning,
-			  args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, throwable, args)
+			  args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, throwable, args)
 	
 	def severe(message: Any,
 			  messageType: String = null,
-			  method: String = null,
 			  className: String = null,
+			  method: String = null,
+			  lineNumber: String = null,
 			  level: LogLevel = LogLevel.Severe,
 			  reference: AnyRef = null,
-			  args: Seq[AnyRef] = null) = Log(message, messageType, method, className, level, reference, args)
+			  args: Seq[AnyRef] = null) = Log(message, messageType, className, method, lineNumber, level, reference, args)
 			  
 	val web = WebLog
 }
