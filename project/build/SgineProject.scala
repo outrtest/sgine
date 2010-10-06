d9752e3a2d092d4be75ac0c4770a429c7e65d000
Matt Hicks <mhicks@sgine.org>
2010-10-06 17:07:53 -0500

    Updates to SBT project to include src/example/resources in test jar.

--- project/build/SgineProject.scala
+++ project/build/SgineProject.scala
@@ -5,6 +5,12 @@ import sbt._
 class SgineProject(info: ProjectInfo) extends DefaultProject(info) {
 	override def testSourceRoots = super.testSourceRoots +++ ("src" / "example")
 	override def runClasspath = super.runClasspath +++ testClasspath +++ ("src" / "main" / "resources") +++ ("src" / "test" / "resources") +++ ("src" / "example" / "resources")
+
+	lazy val exampleSourcePath: Path = ("src" / "example")
+	def exampleResourcesPath = exampleSourcePath / resourcesDirectoryName
+	def exampleResources = descendents(exampleResourcesPath ##, "*")
+	
+	override def packageTestPaths = super.packageTestPaths +++ exampleResources
 	
 	lazy val runExample = task {
 		args => runTask(Some(args(0)), runClasspath).dependsOn(testCompile)
