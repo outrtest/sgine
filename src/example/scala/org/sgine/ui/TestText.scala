8417d1065cf4aca872bddd50a8202017612a954a
Matt Hicks <mhicks@sgine.org>
2010-10-14 11:59:11 -0500

    Added support to Caret and Selection to better operating in rotated state.

--- src/example/scala/org/sgine/ui/TestText.scala
+++ src/example/scala/org/sgine/ui/TestText.scala
@@ -5,17 +5,20 @@ import org.sgine.core._
 import org.sgine.render.Debug
 import org.sgine.render.RenderSettings
 import org.sgine.render.StandardDisplay
+import org.sgine.render.font.FontManager
 
 object TestText extends StandardDisplay with Debug {
 	override val settings = RenderSettings.High
 	
 	def setup() = {
 		val text = new Text()
+		text.font := FontManager("Arial", 64.0)
 		text.focused := true
 		text.editable := true
 		text.textAlignment := "left"
 		text.text := "Now\nis\nthe time for all good men to come to the aid of their country."
-		text.size.width := 300.0
+		text.size.width := 600.0
+		text.rotation.y := 1.0
 		scene += text
 	}
 }
