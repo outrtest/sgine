3649b307a00e25010450443ef3c95bde3e5ba015
Matt Hicks <mhicks@sgine.org>
2010-10-13 19:40:08 -0500

    Fixed positioning flaw in Caret. Updated Text to default to Arial size 36.0. Added Font.style. Added support to FontManager.apply to automatically derive and register a new font if an existing font already exists for that face in a different size.

--- src/main/scala/org/sgine/render/font/FontManager.scala
+++ src/main/scala/org/sgine/render/font/FontManager.scala
@@ -4,8 +4,11 @@ import java.awt.{Font => AWTFont}
 
 import org.sgine.core.Resource
 
+import scala.math._
+
 // TODO: add support for dynamic font generation
 object FontManager {
+	var nearest = false
 	private var fonts: List[Font] = Nil
 	
 	initializeDefaultFonts()
@@ -13,13 +16,59 @@ object FontManager {
 	def apply(awt: AWTFont): Font = apply(awt.getFamily, awt.getSize, awt.getStyle)
 	
 	def apply(face: String, size: Double, style: Int = Font.Plain): Font = {
-		// TODO: add style support
-		fonts.find(f => f.face == face && f.size == size).getOrElse(throw new NullPointerException("Unable to find font: " + face + " " + size))
+		fonts.find(f => f.face == face && f.size == size && f.style == style) match {
+			case Some(f) => f
+			case None => fonts.foldLeft(null.asInstanceOf[Font])(closestSize(face, size)) match {
+				case null => throw new RuntimeException("Unable to find font for face: " + face)
+				case f => {
+					val font = f.derive(size)
+					register(font)
+					font
+				}
+			}
+		}
+	}
+	
+	private def closestSize(face: String, size: Double)(f1: Font, f2: Font) = {
+		if ((f1 != null) && (f2 != null)) {
+			if ((f1.face == face) && (f2.face == face)) {
+				val d1 = if (nearest) {
+					abs(f1.size - size)
+				} else {
+					f1.size - size
+				}
+				val d2 = if (nearest) {
+					abs(f2.size - size)
+				} else {
+					f2.size - size
+				}
+				if ((d1 > 0.0) && (d2 < 0.0)) {
+					f1
+				} else if (d2 > 0.0) {
+					f2
+				} else if (d1 < d2) {
+					f1
+				} else {
+					f2
+				}
+			} else if (f1.face == face) {
+				f1
+			} else if (f2.face == face) {
+				f2
+			} else {
+				null
+			}
+		} else if ((f1 != null) && (f1.face == face)) {
+			f1
+		} else if ((f2 != null) && (f2.face == face)) {
+			f2
+		} else {
+			null
+		}
 	}
 	
 	def register(font: Font) = synchronized {
 		fonts = font :: fonts
-		println("Font: " + font.face + " - " + font.size)
 	}
 	
 	private def initializeDefaultFonts() = {
