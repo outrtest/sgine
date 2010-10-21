988eb69313f187e59ac97b0fb03773afd9dbe9ba
Matt Hicks <mhicks@sgine.org>
2010-10-20 22:01:39 -0500

    Updated IO.stream to support defined length streaming.

--- src/main/scala/org/sgine/util/IO.scala
+++ src/main/scala/org/sgine/util/IO.scala
@@ -3,13 +3,23 @@ package org.sgine.util
 import java.io.InputStream
 import java.io.OutputStream
 
+import scala.math._
+
 object IO {
 	@scala.annotation.tailrec
-	def stream(input: InputStream, output: OutputStream, buf: Array[Byte] = new Array[Byte](512)): Unit = {
-		val len = input.read(buf)
+	def stream(input: InputStream, output: OutputStream, buf: Array[Byte] = new Array[Byte](512), length: Int = -1, written: Int = 0): Unit = {
+		val to = if (length == -1) {
+			buf.length
+		} else {
+			min(buf.length, written - length)
+		}
+		val len = input.read(buf, 0, to)
 		if (len != -1) {
+			val w = written + len
 			output.write(buf, 0, len)
-			stream(input, output, buf)
+			if ((length == -1) || (w < length)) {
+				stream(input, output, buf, length, w)
+			}
 		}
 	}
 }
