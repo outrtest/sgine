cec964bcb15d1765bee06b47146e3dd174476bfe
Matt Hicks <mhicks@sgine.org>
2010-10-23 18:27:32 -0500

    Created ChangeEvent for generic superclass to PropertyChangeEvent. Cleaned up code in PropertyChangeEvent and modified to extend ChangeEvent. Added docs to DependentProperty and added to support to listen to any Listenable and accept ChangeEvent instead of PropertyChangeEvent. Created Component.initialized property to more easily determine the initialized status of a Component. Added additional functionality to Time.waitFor.

--- src/main/scala/org/sgine/util/Time.scala
+++ src/main/scala/org/sgine/util/Time.scala
@@ -116,17 +116,27 @@ object Time {
 	 * is true.
 	 * 
 	 * @param time
+	 * 		The time to wait for the condition to return true.
 	 * @param precision
+	 * 		The recycle period between checks. Defaults to 0.01s.
 	 * @param start
+	 * 		The start time in milliseconds since epoc. Defaults to
+	 * 		System.currentTimeMillis.
+	 * @param errorOnTimeout
+	 * 		If true, throws a java.util.concurrent.TimeoutException upon
+	 * 		timeout. Defaults to false.
 	 * @param condition
+	 * 		The functional condition that must return true.
 	 */
 	@scala.annotation.tailrec
-	def waitFor(time: Double, precision: Long = 10, start: Long = System.currentTimeMillis)(condition: => Boolean): Boolean = {
+	def waitFor(time: Double, precision: Double = 0.01, start: Long = System.currentTimeMillis, errorOnTimeout: Boolean = false)(condition: => Boolean): Boolean = {
+		val p = round(precision * 1000.0)
 		if (!condition) {
 			if ((time >= 0.0) && (System.currentTimeMillis - start > millis(time))) {
+				if (errorOnTimeout) throw new java.util.concurrent.TimeoutException()
 				false
 			} else {
-				Thread.sleep(precision)
+				Thread.sleep(p)
 				
 				waitFor(time, precision, start)(condition)
 			}
