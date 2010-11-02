075c048efaabfc45228a4d71f6d754df13c9cfe5
Matt Hicks <mhicks@sgine.org>
2010-11-01 19:23:08 -0500

    Modified sorting of NodeView to use java.util.Arrays.sort to take advantage of faster sorting (and with jdk7, timsort).

--- src/main/scala/org/sgine/render/scene/RenderSort.scala
+++ src/main/scala/org/sgine/render/scene/RenderSort.scala
@@ -4,19 +4,31 @@ import org.sgine.scene.ext.ColorNode
 import org.sgine.scene.ext.MatrixNode
 import org.sgine.scene.Node
 
-object RenderSort extends Function2[Node, Node, Boolean] {
+object RenderSort extends Function2[Node, Node, Int] {
 	def apply(n1: Node, n2: Node) = {
 		if (n1.isInstanceOf[MatrixNode]) {
 			if (n2.isInstanceOf[MatrixNode]) {
 				val mn1 = n1.asInstanceOf[MatrixNode]
 				val mn2 = n2.asInstanceOf[MatrixNode]
 				
-				mn1.worldMatrix().m23 < mn2.worldMatrix().m23
+				// TODO: implement better sorting:
+				/*
+				 * val dir = entityWorldMatrix(3) - camWorldMatrix(3)
+				 * val distSquare = dot(dir, dir);
+				 */
+				
+				if (mn1.worldMatrix().m23 < mn2.worldMatrix().m23) {
+					-1
+				} else if (mn1.worldMatrix().m23 > mn2.worldMatrix().m23) {
+					1
+				} else {
+					0
+				}
 			} else {
-				false
+				-1
 			}
 		} else {
-			true
+			1
 		}
 	}
 }
