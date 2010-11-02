7d4df6286b52a89ec0beb0e842f7af9ff7bcc4e1
Matt Hicks <mhicks@sgine.org>
2010-11-02 12:45:22 -0500

    Fixed a bug in util.IO class

--- src/main/scala/org/sgine/util/IO.scala
+++ src/main/scala/org/sgine/util/IO.scala
@@ -7,11 +7,11 @@ import scala.math._
 
 object IO {
 	@scala.annotation.tailrec
-	def stream(input: InputStream, output: OutputStream, buf: Array[Byte] = new Array[Byte](512), length: Int = -1, written: Int = 0): Unit = {
+	def stream(input: InputStream, output: OutputStream, buf: Array[Byte] = new Array[Byte](4096), length: Int = -1, written: Int = 0): Unit = {
 		val to = if (length == -1) {
 			buf.length
 		} else {
-			min(buf.length, written - length)
+			min(buf.length, length - written)
 		}
 		val len = input.read(buf, 0, to)
 		if (len != -1) {
