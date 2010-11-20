10dbaaa2f145b23e91e5a01f7d1c84f32fb2250b
Matt Hicks <mhicks@sgine.org>
2010-11-20 11:11:46 -0600

    Added invokeLater convenience methods to process package to allow delayed invocation. Added shutdown support to Updatable. Added "count" support to Updatable to allow invoking a specified number of times.

--- src/main/scala/org/sgine/process/package.scala
+++ src/main/scala/org/sgine/process/package.scala
@@ -1,7 +1,24 @@
 package org.sgine
 
+import java.util.Calendar
+
 package object process {
-	def update(rate: Double = 0.01)(f: => Unit) = Updatable(rate)(f)
+	def update(rate: Double = 0.01, count: Int = -1)(f: => Unit) = Updatable(rate, count)(f)
+	
+	def invokeLater(distance: Double)(f: => Unit) = new Updatable {
+		val rate = distance
+		val count = -1
+		def update(time: Double) = {
+			f
+			shutdown()
+		}
+	}
+	
+	def invokeLater(calendar: Calendar)(f: => Unit): Unit = {
+		val now = Calendar.getInstance
+		val distance = (now.getTimeInMillis - calendar.getTimeInMillis) / 1000.0
+		invokeLater(distance)(f)
+	}
 	
 	def attempt(f: => Unit): Boolean = Process(() => f, ProcessHandling.Attempt)
 	
