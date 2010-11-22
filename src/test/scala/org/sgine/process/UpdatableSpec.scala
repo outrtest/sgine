00f9ac6671276e706e9d2ce5f6abaf7a8b2db877
Matt Hicks <mhicks@sgine.org>
2010-11-21 20:33:50 -0600

    Created Executor to execute queued functionality asynchronously in FIFO order utilizing Updatable. Minor structural updates to Updatable. Added convenience method on package object for "executor".

--- src/test/scala/org/sgine/process/UpdatableSpec.scala
+++ src/test/scala/org/sgine/process/UpdatableSpec.scala
@@ -4,6 +4,8 @@ import org.scalatest.FlatSpec
 
 import org.scalatest.matchers.ShouldMatchers
 
+import org.sgine.core._
+
 import org.sgine.util.Time
 
 class UpdatableSpec extends FlatSpec with ShouldMatchers {
@@ -20,14 +22,53 @@ class UpdatableSpec extends FlatSpec with ShouldMatchers {
 	
 	it should "invokeLater" in {
 		var invoked = false
-		invokeLater(2.0) {
+		invokeLater(1.0) {
 			invoked = true
 		}
-		Thread.sleep(1000)
+		Thread.sleep(500)
 		invoked should equal(false)
-		Time.waitFor(2.0) {
+		Time.waitFor(0.6) {
 			invoked
 		}
 		invoked should equal(true)
+	}
+	
+	it should "properly execute" in {
+		val value = new java.util.concurrent.atomic.AtomicInteger(0)
+		val adder = executor[Int] {(v: Int) => value.addAndGet(v)}
+		adder += 3
+		adder += 2
+		adder += 4
+		adder += 1
+		
+		Time.waitFor(5.0) {
+			value.get == 10
+		}
+		value.get should equal(10)
+		
+		adder += 5
+		
+		Time.waitFor(5.0) {
+			value.get == 15
+		}
+		value.get should equal(15)
+	}
+	
+	it should "execute in the right order and synchronously" in {
+		var value = "Hello"
+		val concatenator = executor[String] {(v: String) => value += v}
+		concatenator += " This"
+		concatenator += " is"
+		concatenator += " in"
+		concatenator += " the"
+		concatenator += " right"
+		Thread.sleep(1000)
+		concatenator += " order."
+		
+		val correct = "Hello This is in the right order."
+		Time.waitFor(5.0) {
+			value == correct
+		}
+		value should equal(correct)
 	}
 }
