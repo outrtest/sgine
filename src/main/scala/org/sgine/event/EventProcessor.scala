c7d6b49a200527138460c4e9a417117e2a974f46
Matt Hicks <mhicks@imirus.com>
2011-01-27 13:05:21 -0600

    Modified EventProcessor.handlers to be transient so listeners don't get persisted.

--- src/main/scala/org/sgine/event/EventProcessor.scala
+++ src/main/scala/org/sgine/event/EventProcessor.scala
@@ -4,7 +4,7 @@ import scala.reflect.Manifest
 
 @serializable
 final class EventProcessor(listenable: Listenable) extends Iterable[EventHandler] {
-	private var handlers: List[EventHandler] = Nil
+	@transient private var handlers: List[EventHandler] = Nil
 	
 	def +=[E <: Event](listener: E => Unit)(implicit manifest: Manifest[E]): EventHandler = {
 		val h = EventHandler(EventListener(listener))
@@ -14,6 +14,7 @@ final class EventProcessor(listenable: Listenable) extends Iterable[EventHandler
 	def +=(handler: EventHandler): EventHandler = {
 		if (handler == null) throw new NullPointerException("EventHandler must not be null")
 		synchronized {
+      if (handlers == null) handlers = Nil
 			if (!handlers.contains(handler)) {
 				handlers = handler :: handlers
 			}
@@ -24,11 +25,15 @@ final class EventProcessor(listenable: Listenable) extends Iterable[EventHandler
 	def -=(handler: EventHandler): EventHandler = {
 		synchronized {
 			if (handler != null) {
+        if (handlers == null) handlers = Nil
 				handlers = handlers.filterNot(_ == handler)
 			}
 		}
 		handler
 	}
 	
-	def iterator = handlers.iterator
+	def iterator = {
+    if (handlers == null) handlers = Nil
+    handlers.iterator
+  }
 }
