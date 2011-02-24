80c6a40407bb6d710bef167363b1d9e61258ed37
mhicks <mhicks@sgine.org>
2011-02-23 18:59:00 -0700

    Resolves issue #6
    Added android.jar to generator/lib for now until I can determine a way to make it a managed dependency.
    Added Scala Code Coverage plugin for SBT to project.

--- project/build/SgineProject.scala
+++ project/build/SgineProject.scala
@@ -1,11 +1,21 @@
 import sbt._
 
+import reaktor.scct.ScctProject
+
 class SgineProject(info: ProjectInfo) extends ParentProject(info) {
-  lazy val core = project("core", "core")
+  val lwjglRepo = "LWJGL Repository" at "http://adterrasperaspera.com/lwjgl/"
+
+  lazy val core = project("core", "core", new CoreProject(_))
   lazy val opengl = project("opengl", "opengl", new OpenGLProject(_), core)
   lazy val render = project("render", "render", core, opengl)
 
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject
+
   class OpenGLProject(info: ProjectInfo) extends ParentProject(info) {
-    lazy val generator = project("generator", "generator", core)
+    lazy val generator = project("generator", "generator", new OpenGLGeneratorProject(_), core)
+  }
+
+  class OpenGLGeneratorProject(info: ProjectInfo) extends DefaultProject(info) {
+    lazy val lwjgl = "org.lwjgl" % "lwjgl" % "2.7.1"
   }
 }
