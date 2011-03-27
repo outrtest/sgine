7185b41ad9f111ce9db1241482dfa6e4921d7284
mhicks <mhicks@mhicks-pc>
2011-03-27 16:49:04 -0600

    Created SBT projects for LWJGL and Android. Created SBT project for "api" in "opengl" to allow dependency references to exist.

--- project/build/SgineProject.scala
+++ project/build/SgineProject.scala
@@ -4,6 +4,7 @@ import reaktor.scct.ScctProject
 
 class SgineProject(info: ProjectInfo) extends ParentProject(info) {
   val lwjglRepo = "LWJGL Repository" at "http://adterrasperaspera.com/lwjgl/"
+  val lwjglVersion = "2.7.1"
 
   lazy val core = project("core", "core", new CoreProject(_))
   lazy val opengl = project("opengl", "opengl", new OpenGLProject(_), core)
@@ -13,10 +14,18 @@ class SgineProject(info: ProjectInfo) extends ParentProject(info) {
 
   class OpenGLProject(info: ProjectInfo) extends ParentProject(info) {
     lazy val generator = project("generator", "generator", new OpenGLGeneratorProject(_), core)
+    lazy val api = project("api", "api", core)
+    lazy val lwjgl = project("lwjgl", "lwjgl", new OpenGLLWJGLProject(_), api)
+    lazy val android = project("android", "android", new OpenGLAndroidProject(_), api)
   }
 
   class OpenGLGeneratorProject(info: ProjectInfo) extends DefaultProject(info) {
-    lazy val lwjgl = "org.lwjgl" % "lwjgl" % "2.7.1"
-    lazy val paranamer  = "com.thoughtworks.paranamer" % "paranamer" % "2.2.1"
+    lazy val lwjgl = "org.lwjgl" % "lwjgl" % lwjglVersion
   }
+
+  class OpenGLLWJGLProject(info: ProjectInfo) extends DefaultProject(info) {
+    lazy val lwjgl = "org.lwjgl" % "lwjgl" % lwjglVersion
+  }
+
+  class OpenGLAndroidProject(info: ProjectInfo) extends DefaultProject(info)
 }
