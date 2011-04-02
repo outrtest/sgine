d379653a6d1dcd1e992875bac9c1d26e5dc7f0be
mhicks <mhicks@mhicks-pc>
2011-04-02 09:17:06 -0600

    Modified SBT version to 0.7.5 release. Updated SgineProject to reference more descriptive names for generated resources.

--- project/build/SgineProject.scala
+++ project/build/SgineProject.scala
@@ -6,19 +6,19 @@ class SgineProject(info: ProjectInfo) extends ParentProject(info) {
   val lwjglRepo = "LWJGL Repository" at "http://adterrasperaspera.com/lwjgl/"
   val lwjglVersion = "2.7.1"
 
-  lazy val concurrent = project("concurrent", "concurrent", core)
-  lazy val core = project("core", "core", new CoreProject(_))
-  lazy val event = project("event", "event", core)
-  lazy val opengl = project("opengl", "opengl", new OpenGLProject(_), core)
-  lazy val render = project("render", "render", core, opengl)
+  lazy val concurrent = project("concurrent", "sgine-concurrent", core)
+  lazy val core = project("core", "sgine-core", new CoreProject(_))
+  lazy val event = project("event", "sgine-event", core)
+  lazy val opengl = project("opengl", "sgine-opengl", new OpenGLProject(_), core)
+  lazy val render = project("render", "sgine-render", core, opengl)
 
   class CoreProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject
 
   class OpenGLProject(info: ProjectInfo) extends ParentProject(info) {
-    lazy val generator = project("generator", "generator", new OpenGLGeneratorProject(_), core)
-    lazy val api = project("api", "api", core)
-    lazy val lwjgl = project("lwjgl", "lwjgl", new OpenGLLWJGLProject(_), api)
-    lazy val android = project("android", "android", new OpenGLAndroidProject(_), api)
+    lazy val generator = project("generator", "sgine-opengl-generator", new OpenGLGeneratorProject(_), core)
+    lazy val api = project("api", "sgine-opengl-api", core)
+    lazy val lwjgl = project("lwjgl", "sgine-opengl-lwjgl", new OpenGLLWJGLProject(_), api)
+    lazy val android = project("android", "sgine-opengl-android", new OpenGLAndroidProject(_), api)
   }
 
   class OpenGLGeneratorProject(info: ProjectInfo) extends DefaultProject(info) {
