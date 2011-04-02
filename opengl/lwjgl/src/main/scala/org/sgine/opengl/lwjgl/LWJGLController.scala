6ce8442518d554f033b590bf68eeda9e7cb7423b
mhicks <mhicks@mhicks-pc>
2011-04-02 11:35:29 -0600

    Added copyright information to all source files. Created GLController to be implemented by GL implementations. Created new Property sub-project.

--- opengl/lwjgl/src/main/scala/org/sgine/opengl/lwjgl/LWJGLController.scala
+++ opengl/lwjgl/src/main/scala/org/sgine/opengl/lwjgl/LWJGLController.scala
@@ -30,64 +30,22 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.sgine
+package org.sgine.opengl.lwjgl
 
-import util.Random
+import java.awt.Canvas
+import org.sgine.opengl.{GLController, GLDisplay}
+import java.awt.event.{ComponentEvent, ComponentAdapter}
+import java.util.concurrent.atomic.AtomicBoolean
 
-/**
- * Enumerated must be mixed into the companion object for Enum implementation.
- */
-trait Enumerated[E <: Enum] extends Traversable[E] {
-  private lazy val r = new Random()
+class LWJGLController(canvas: Canvas, display: GLDisplay) extends GLController {
+  // Set to true when the AWT canvas has been resized
+  private val resized = new AtomicBoolean()
 
-  /**
-   * Sequence of all associated Enums
-   */
-  def values: Seq[E]
+  canvas.addComponentListener(new ComponentAdapter() {
+    override def componentResized(evt: ComponentEvent) = {
+      resized.set(true)
+    }
+  })
 
-  /**
-   * Number of Enums
-   */
-  override def size = values.size
-
-  /**
-   * Retrieve Enum by index
-   */
-  def apply(index: Int) = values(index)
-
-  /**
-   * Next Enum from current.
-   *
-   * @param wrap Wraps around back to the beginning if true. Defaults to true.
-   */
-  def next(current: E, wrap: Boolean = true) = current.ordinal match {
-    case index if (index == size - 1) => head
-    case index => apply(index)
-  }
-
-  /**
-   * Previous Enum from current.
-   *
-   * @param wrap Wraps around back to the end if true. Defaults to true.
-   */
-  def previous(current: E, wrap: Boolean = true) = current.ordinal match {
-    case 0 => last
-    case index => apply(index)
-  }
-
-  /**
-   * Retrieves a random Enum.
-   */
-  def random = values(r.nextInt(size))
-
-  def foreach[U](f: E => U) = values.foreach(f)
-
-  def valueOf(s: String) = values.find(e => e.name == s)
-
-  def indexOf(e: E) = values.indexOf(e)
-
-  def indexOf(s: String): Int = valueOf(s) match {
-    case Some(e) => indexOf(e)
-    case None => -1
-  }
+  
 }
--- project/build/SgineProject.scala
+++ project/build/SgineProject.scala
@@ -6,17 +6,18 @@ class SgineProject(info: ProjectInfo) extends ParentProject(info) {
   val lwjglRepo = "LWJGL Repository" at "http://adterrasperaspera.com/lwjgl/"
   val lwjglVersion = "2.7.1"
 
-  lazy val concurrent = project("concurrent", "sgine-concurrent", core)
+  lazy val concurrent = project("concurrent", "sgine-concurrent")
   lazy val core = project("core", "sgine-core", new CoreProject(_))
-  lazy val event = project("event", "sgine-event", core)
-  lazy val opengl = project("opengl", "sgine-opengl", new OpenGLProject(_), core)
-  lazy val render = project("render", "sgine-render", core, opengl)
+  lazy val event = project("event", "sgine-event")
+  lazy val opengl = project("opengl", "sgine-opengl", new OpenGLProject(_))
+  lazy val property = project("property", "sgine-property")
+  lazy val render = project("render", "sgine-render", opengl)
 
   class CoreProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject
 
   class OpenGLProject(info: ProjectInfo) extends ParentProject(info) {
-    lazy val generator = project("generator", "sgine-opengl-generator", new OpenGLGeneratorProject(_), core)
-    lazy val api = project("api", "sgine-opengl-api", core)
+    lazy val generator = project("generator", "sgine-opengl-generator", new OpenGLGeneratorProject(_))
+    lazy val api = project("api", "sgine-opengl-api")
     lazy val lwjgl = project("lwjgl", "sgine-opengl-lwjgl", new OpenGLLWJGLProject(_), api)
     lazy val android = project("android", "sgine-opengl-android", new OpenGLAndroidProject(_), api)
   }
