a53e1ca35fc6a3cad42cf6450972390d090fc11e
mhicks <mhicks@sgine.org>
2011-05-27 08:00:03 -0600

    Added ScalaTest, fixed minor bug in property package, and created tests for Enumerated.

---
+++ core/src/test/scala/org/sgine/EnumeratedSpec.scala
@@ -0,0 +1,35 @@
+package org.sgine
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class EnumeratedSpec extends FlatSpec with ShouldMatchers {
+  "TestEnum" should "have three entries" in {
+    TestEnum.size should be(3)
+  }
+
+  it should  "have One as the first entry" in {
+    TestEnum.One.ordinal should be(0)
+    TestEnum(0) should be(TestEnum.One)
+  }
+
+  it should  "have Two as the second entry" in {
+    TestEnum(1) should be(TestEnum.Two)
+    TestEnum.Two.ordinal should be(1)
+  }
+
+  it should  "have Three as the third entry" in {
+    TestEnum(2) should be(TestEnum.Three)
+    TestEnum.Three.ordinal should be(2)
+  }
+}
+
+sealed trait TestEnum extends EnumEntry
+
+object TestEnum extends Enumerated[TestEnum] {
+  case object One extends TestEnum
+  case object Two extends TestEnum
+  case object Three extends TestEnum
+
+  val values = List(One, Two, Three)
+}
