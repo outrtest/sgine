602bfea5ca7fc2a3225111e2fcd3625e964314be
mhicks <mhicks@localhost>
2011-11-07 18:40:01 -0700

    Set default values on Component. Updates to RenderableComponent.draw method. Removed enforcement of POT textures.

--- ui/src/main/scala/org/sgine/ui/RenderableComponent.scala
+++ ui/src/main/scala/org/sgine/ui/RenderableComponent.scala
@@ -1,10 +1,17 @@
 package org.sgine.ui
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
 /**
  * RenderableComponent is mixed into Components that render something to the screen.
  *
  * @author Matt Hicks <mhicks@sgine.org>
  */
 trait RenderableComponent extends Component {
-  def render(): Unit
+  final def render() = {
+    val batch = Component.batch.get()
+    draw(batch)
+  }
+
+  protected def draw(batch: SpriteBatch): Unit
 }
