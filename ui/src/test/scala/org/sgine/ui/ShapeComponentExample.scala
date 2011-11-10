820f17b686479b4ccb7f1a9310ba1baae440fc37
Matt Hicks <mhicks@sgine.org>
2011-11-10 08:52:13 -0700

    Created companion object for easily constructing an Image. Removed GLTest. Created ImageExample and ShapeComponentExample to represent examples of the corresponding components.

---
+++ ui/src/test/scala/org/sgine/ui/ShapeComponentExample.scala
@@ -0,0 +1,20 @@
+package org.sgine.ui
+
+import com.badlogic.gdx.graphics.Texture
+import org.sgine.Resource
+import render.{Vertex, TextureCoordinates}
+
+/**
+ *
+ *
+ * @author Matt Hicks <mhicks@sgine.org>
+ */
+object ShapeComponentExample extends UI {
+  val texture = new Texture(Resource("sgine.png"))
+
+  val shape = new ShapeComponent()
+  shape.texture := texture
+  shape.textureCoordinates := TextureCoordinates.rectCoords(0.0, 0.0, 400.0, 96.0, texture)
+  shape.vertices := Vertex.rect(400.0, 96.0)
+  contents += shape
+}
