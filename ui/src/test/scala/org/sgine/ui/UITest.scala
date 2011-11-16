44de2295daaf301f214086df63259ee423a8468f
Matt Hicks <mhicks@sgine.org>
2011-11-16 07:49:34 -0700

    Refactored Label to build on TextComponent and updated LabelExample.

--- ui/src/test/scala/org/sgine/ui/UITest.scala
+++ ui/src/test/scala/org/sgine/ui/UITest.scala
@@ -1,30 +1,9 @@
 package org.sgine.ui
 
-import org.sgine.input.event.MouseOverEvent
-
 /**
  *
  *
  * @author Matt Hicks <mhicks@sgine.org>
  */
 object UITest extends UI {
-  val label = new Label()
-  label.location.x := 300.0
-  label.location.y := 500.0
-  label.text := "Hello World!"
-  //  label.font := font
-  //  contents += label
-
-  val text = new Label()
-  text.location.x := 600.0
-  text.location.y := 300.0
-  text.wrapWidth := 150.0
-  text.text :=
-      "This is lots of text that should wrap to multiple lines properly. Let's see if it does.\n\nThis should have broken to a new line...did it?"
-  text.font := label.font()
-  text.mouseEvent.synchronous {
-    case evt: MouseOverEvent => println("Label: " + evt)
-    case _ =>
-  }
-  //  contents += text
 }
