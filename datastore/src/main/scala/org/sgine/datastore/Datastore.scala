25b0014f0d8c6ce5ea37de2dbf37ef037e5cafff
mhicks <mhicks@mhicks-pc>
2012-02-23 15:13:57 -0600

    Updated datastore to be more flexible and added support for MongoDB as a datastore implementation. Added support to datastore to throw events.

--- datastore/src/main/scala/org/sgine/datastore/Datastore.scala
+++ datastore/src/main/scala/org/sgine/datastore/Datastore.scala
@@ -1,11 +1,23 @@
 package org.sgine.datastore
 
+import event.{DatastorePersist, DatastoreDelete}
+import org.sgine.event.Listenable
+import java.util.UUID
+
 /**
  * Datastore implements functionality for working with a backing datastore.
  *
  * @author Matt Hicks <mhicks@sgine.org>
  */
-trait Datastore {
+trait Datastore[T <: Identifiable] extends Listenable {
+  def manifest: Manifest[T]
+
+  def layers: Seq[Datastore[_]]
+
+  protected def persistInternal(obj: T): Unit
+
+  protected def deleteInternal(id: UUID): Boolean
+
   def transaction(f: => Unit) = try {
     f
     commit()
@@ -16,51 +28,100 @@ trait Datastore {
     }
   }
 
-  def persist(obj: AnyRef): Unit
+  def canUse(obj: AnyRef) = manifest.erasure.isAssignableFrom(obj.getClass)
 
-  def persistAll(objects: AnyRef*) = objects.foreach(persist)
+  def persist(obj: T): Unit = {
+    persistInternal(obj)
+    fire(DatastorePersist(this, obj))
 
-  def delete(obj: AnyRef): Unit
+    layers.foreach(layer => if (layer.canUse(obj)) {
+      layer.asInstanceOf[Datastore[T]].persist(obj)
+    })
+  }
+
+  /**
+   * Removes the object by the same id in the datastore before persisting this one.
+   *
+   * @param obj to persist
+   */
+  def update(obj: T): Unit = {
+    byId(obj.id) match {
+      case Some(instance) => delete(instance)
+      case None =>
+    }
+    persist(obj)
+
+    layers.foreach(layer => if (layer.canUse(obj)) {
+      layer.asInstanceOf[Datastore[T]].update(obj)
+    })
+  }
+
+  def persistAll(objects: T*) = objects.foreach(persist)
+
+  def delete(obj: T): Boolean = if (deleteInternal(obj.id)) {
+    fire(DatastoreDelete(this, obj))
+    layers.foreach(layer => if (layer.canUse(obj)) {
+      layer.asInstanceOf[Datastore[T]].delete(obj)
+    })
+    true
+  } else {
+    false
+  }
 
   def commit(): Unit
 
   def rollback(): Unit
 
-  def byExample[T](obj: T)(implicit manifest: Manifest[T]): Seq[T]
+  def byExample(obj: T): Iterator[T]
 
-  def all[T]()(implicit manifest: Manifest[T]): Seq[T]
+  def byId(id: UUID): Option[T] = firstOption(obj => obj.id == id)
 
-  def query[T](matcher: T => Boolean)(implicit manifest: Manifest[T]): Seq[T]
-
-  def close(): Unit
-
-  def firstByExample[T](obj: T)(implicit manifest: Manifest[T]) = byExample[T](obj)(manifest) match {
-    case results if (results.isEmpty) => null.asInstanceOf[T]
-    case results => results.head
+  def deleteById(id: UUID) = {
+    byId(id) match {
+      case Some(instance) => delete(instance)
+      case None => false
+    }
   }
 
-  def firstOptionByExample[T](obj: T)(implicit manifest: Manifest[T]) = firstByExample[T](obj)(manifest) match {
+  def all: Iterator[T]
+
+  /**
+   * Queries the store for entries matching 'matcher' as a criteria.
+   *
+   * NOTE: For performance reasons this method should generally be overridden by implementations.
+   *
+   * @param matcher is used to limit the results
+   * @return Iterator[T]
+   */
+  def query(matcher: T => Boolean): Iterator[T] = all.filter(matcher)
+
+  def firstByExample(obj: T) = byExample(obj) match {
+    case results if (results.isEmpty) => null.asInstanceOf[T]
+    case results => results.next()
+  }
+
+  def firstOptionByExample(obj: T) = firstByExample(obj) match {
     case null => None
     case result => Some(result)
   }
 
-  def first[T]()(implicit manifest: Manifest[T]) = all[T]()(manifest) match {
+  def first() = all match {
     case results if (results.isEmpty) => null.asInstanceOf[T]
-    case results => results.head
+    case results => results.next()
   }
 
-  def firstOption[T]()(implicit manifest: Manifest[T]) = all[T]()(manifest) match {
+  def firstOption() = all match {
     case results if (results.isEmpty) => None
-    case results => Some(results.head)
+    case results => Some(results.next())
   }
 
-  def first[T](matcher: T => Boolean)(implicit manifest: Manifest[T]) = query(matcher)(manifest) match {
+  def first(matcher: T => Boolean) = query(matcher) match {
     case results if (results.isEmpty) => null.asInstanceOf[T]
-    case results => results.head
+    case results => results.next()
   }
 
-  def firstOption[T](matcher: T => Boolean)(implicit manifest: Manifest[T]) = query(matcher)(manifest) match {
+  def firstOption(matcher: T => Boolean) = query(matcher) match {
     case results if (results.isEmpty) => None
-    case results => Some(results.head)
+    case results => Some(results.next())
   }
 }
