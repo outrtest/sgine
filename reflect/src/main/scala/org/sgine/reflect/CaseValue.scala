54b388c0d866c2e88436252a6b42e4ac8dd30d68
Matt Hicks <matt@outr.com>
2012-03-26 16:39:50 -0500

    Minor update to CaseValue

--- reflect/src/main/scala/org/sgine/reflect/CaseValue.scala
+++ reflect/src/main/scala/org/sgine/reflect/CaseValue.scala
@@ -11,9 +11,11 @@ case class CaseValue(name: String, valueType: EnhancedClass, clazz: EnhancedClas
   lazy val getter = clazz.method(name)
   lazy val setter = clazz.methods.find(m => m.name == "%s_$eq".format(name))
 
+  private lazy val field = clazz.javaClass.getDeclaredField(name)
+
   def isMutable = setter != None
 
-  def isTransient = Modifier.isTransient(clazz.javaClass.getDeclaredField(name).getModifiers)
+  def isTransient = Modifier.isTransient(field.getModifiers)
 
   def apply[T](instance: AnyRef) = getter.get.invoke[T](instance)
 
