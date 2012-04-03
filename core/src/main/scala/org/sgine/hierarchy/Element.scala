487967f2709be59070b448993f8497709fc16c94
mhicks <mhicks@mhicks-laptop>
2012-04-02 21:07:25 -0500

    Added descendant and ancestor functions to Element.

--- core/src/main/scala/org/sgine/hierarchy/Element.scala
+++ core/src/main/scala/org/sgine/hierarchy/Element.scala
@@ -55,11 +55,81 @@ trait Element {
      * The last element hierarchically from this level.
      */
     def last = Element.this
+
+    /**
+     * Returns true if the value passed is in the ancestry hierarchy for this Child.
+     */
+    def hasAncestor[T](value: T, maxDepth: Int = Int.MaxValue)(implicit manifest: Manifest[T]) = ancestor((t: T) => t == value, maxDepth)(manifest) != None
+
+    /**
+     * Returns true if the value passed is the parent of this Child.
+     */
+    def hasParent[T](value: T)(implicit manifest: Manifest[T]) = hasAncestor(value, 1)(manifest)
+
+    /**
+     * Uses the supplied matching function to return the first ancestor match given the specified type or None if no
+     * match is found.
+     */
+    def ancestor[T](matcher: T => Boolean, maxDepth: Int = Int.MaxValue)(implicit manifest: Manifest[T]): Option[T] = {
+      val p = parent
+      if (p != null) {
+        if (manifest.erasure.isAssignableFrom(p.asInstanceOf[AnyRef].getClass) && matcher(p.asInstanceOf[T])) {
+          Option(p.asInstanceOf[T])
+        } else if (maxDepth > 1) {
+          p.hierarchy.ancestor[T](matcher, maxDepth - 1)(manifest)
+        } else {
+          None
+        }
+      } else {
+        None
+      }
+    }
+
+    /**
+     * Processes up the ancestry tree through parents to find the first matching ancestor of the
+     * generic type T and invokes the supplied function on it.
+     */
+    def ancestor[T](f: T => Unit)(implicit manifest: Manifest[T]): Unit = {
+      val p = parent
+      if (p != null) {
+        if (manifest.erasure.isAssignableFrom(p.getClass)) {
+          f(p.asInstanceOf[T])
+        } else {
+          p.hierarchy.ancestor(f)(manifest)
+        }
+      }
+    }
+
+    /**
+     * Processes up the ancestry tree through parents executing the supplied function on all parents
+     * that match the supplied generic type.
+     */
+    def ancestors[T](f: T => Unit)(implicit manifest: Manifest[T]): Unit = {
+      val p = parent
+      if (p != null) {
+        if (manifest.erasure.isAssignableFrom(p.getClass)) {
+          f(p.asInstanceOf[T])
+        }
+        p.hierarchy.ancestor(f)(manifest)
+      }
+    }
+
+    /**
+     * Invokes the supplied method if this class matches the supplied generic type.
+     *
+     * Returns true if the generic type matched.
+     */
+    def apply[T](f: T => Unit)(implicit manifest: Manifest[T]): Boolean = {
+      if (manifest.erasure.isAssignableFrom(getClass)) {
+        f(this.asInstanceOf[T])
+        true
+      }
+      else {
+        false
+      }
+    }
   }
 
-}
-
-object Element {
 }
 
 trait Parent extends Element {
@@ -82,6 +152,42 @@ trait Parent extends Element {
       children.last.hierarchy.last
     } else {
       super.last
+    }
+
+    /**
+     * Returns true if the value passed is in the descendant hierarchy for this Parent.
+     */
+    def hasDescendant[T](value: T, maxDepth: Int = Int.MaxValue)(implicit manifest: Manifest[T]) = descendant((t: T) => t == value, maxDepth)(manifest) != None
+
+    /**
+     * Returns true if the value passed is a child of this Parent.
+     */
+    def hasChild[T](value: T)(implicit manifest: Manifest[T]) = hasDescendant(value, 1)(manifest)
+
+    /**
+     * Uses the supplied matching function to return the first descendant match given the specified type or None if no
+     * match is found.
+     */
+    def descendant[T](matcher: T => Boolean, maxDepth: Int = Int.MaxValue, children: Seq[Element] = Parent.this.children)(implicit manifest: Manifest[T]): Option[T] = {
+      if (children.nonEmpty) {
+        // if (manifest.erasure.isAssignableFrom(p.getClass) && matcher(p.asInstanceOf[T])) {
+        val child = children.head
+        if (manifest.erasure.isAssignableFrom(child.asInstanceOf[AnyRef].getClass) && matcher(child.asInstanceOf[T])) {
+          Option(child.asInstanceOf[T])
+        } else {
+          val result = descendant(matcher, maxDepth, children.tail)(manifest)
+          if (result == None && maxDepth > 1) {
+            child match {
+              case parent: Parent => parent.hierarchy.descendant(matcher, maxDepth - 1, parent.children)
+              case _ => None
+            }
+          } else {
+            result
+          }
+        }
+      } else {
+        None
+      }
     }
 
     @tailrec
