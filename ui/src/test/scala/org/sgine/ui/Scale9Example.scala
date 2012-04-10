32256adf5bb986b4894a95397d9fad74e54397ff
Matt Hicks <matt@outr.com>
2012-04-10 10:01:00 -0500

    Created Scale9 supporting component.

--- ui/src/test/scala/org/sgine/ui/Scale9Example.scala
+++ ui/src/test/scala/org/sgine/ui/Scale9Example.scala
@@ -1,18 +1,12 @@
 package org.sgine.ui
 
-import com.badlogic.gdx.graphics.Texture
 import org.sgine.Resource
-import render.{Vertex, TextureCoordinates}
 
 /**
  * @author Matt Hicks <mhicks@sgine.org>
  */
 object Scale9Example extends UI {
-  val texture = new Texture(Resource("scale9test.png"))
-
-  val shape = new ShapeComponent()
-  shape._texture := texture
-  shape._vertices := Vertex.scale9(50.0, 50.0, 450.0, 450.0, 0.0, 500.0, 500.0, 200.0, 500.0)
-  shape._textureCoordinates := TextureCoordinates.scale9(50.0, 50.0, 450.0, 450.0, 500.0, 500.0)
-  contents += shape
+  val scale9 = Scale9(Resource("scale9test.png"), 50.0, 50.0, 450.0, 450.0)
+  scale9.size.width := 200.0
+  contents += scale9
 }
