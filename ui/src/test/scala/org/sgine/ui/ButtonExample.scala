66743debab69d46800999c45ae5b9b41bb8ca005
Matt Hicks <matt@outr.com>
2012-04-17 10:40:11 -0500

    Added Component.includeInLayout property to allow excluding components from container layout. Added AbstractContainer updateLayout() and default implementation to update the absolute layout (update the size of container based on children). Added Component.padding to allow padding top, bottom, left, and right of component. Implemented padding support in both container and component. Further progress on Button component.

--- ui/src/test/scala/org/sgine/ui/ButtonExample.scala
+++ ui/src/test/scala/org/sgine/ui/ButtonExample.scala
@@ -1,8 +1,7 @@
 package org.sgine.ui
 
-import align.HorizontalAlignment
 import font.BitmapFont
-import org.sgine.Resource
+import org.sgine.{Color, Resource}
 
 
 /**
@@ -10,30 +9,24 @@ import org.sgine.Resource
  */
 object ButtonExample extends UI with Debug {
   val button = new Button()
-  button.alignment.horizontal := HorizontalAlignment.Left
+  button.padding(20.0, 20.0, 20.0, 20.0)
   contents += button
 
   button.label.text := "Hello World!"
+  button.label.color(Color.Black)
 }
 
 class Button extends AbstractContainer {
-  val background = new Scale9(Resource("scale9test.png"), 50.0, 50.0, 450.0, 450.0)
+  val background = new Scale9(Resource("scale9/windows/button/normal.png"), 3.0, 3.0, 4.0, 5.0)
+  background.includeInLayout := false
   addChild(background)
 
   implicit val font = BitmapFont(Resource("arial64.fnt"))
   val label = Label("")
-  label.size.width.listeners.synchronous {
-    case evt => println("Width changed! " + label.size.width.name)
-  }
   label.location.z := 0.01
   addChild(label)
 
-  println(label.size.width.parent)
-  println(label.size.width.name)
-
-//  onChange(label.size.width, label.size.height) {
-//    println("Label size changed!")
-//    background.size.width := label.size.width()
-//    background.size.height := label.size.height()
-//  }
+  onChange(size.width, size.height) {
+    background.size(size.width(), size.height())
+  }
 }
