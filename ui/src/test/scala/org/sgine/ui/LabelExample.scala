e55e32dde4926ebf45f3f085735b19ca290cfa3c
Matt Hicks <matt@outr.com>
2012-04-21 21:58:24 -0500

    Reverted changes to TextureCoordinates. Added support for launching application via configuration.

--- ui/src/test/scala/org/sgine/ui/LabelExample.scala
+++ ui/src/test/scala/org/sgine/ui/LabelExample.scala
@@ -1,7 +1,5 @@
 package org.sgine.ui
 
-import font.BitmapFont
-import org.sgine.Resource
 
 /**
  *
@@ -9,6 +7,5 @@ import org.sgine.Resource
  * @author Matt Hicks <mhicks@sgine.org>
  */
 object LabelExample extends UI {
-  implicit val font = BitmapFont(Resource("arial64.fnt"))
-  contents += Label("Hello World!")
+  contents += new Label("Hello World!")
 }
