dd36d0f82b548af3f1e5bc3a60f80e830440bdf1
Matt Hicks <matt@outr.com>
2012-04-26 15:08:56 -0500

    Updated ObjectPropertyParent to no longer use getClass.getSimpleName (as it is buggy), but to use getName instead. Added basic Vertex.clip support.

--- property/src/main/scala/org/sgine/property/ObjectPropertyParent.scala
+++ property/src/main/scala/org/sgine/property/ObjectPropertyParent.scala
@@ -4,7 +4,8 @@ package org.sgine.property
  * @author Matt Hicks <mhicks@sgine.org>
  */
 class ObjectPropertyParent(val parent: PropertyParent) extends PropertyParent {
-  val name = getClass.getSimpleName.substring(0, getClass.getSimpleName.length - 1) match {
+  private val simpleName = getClass.getName.substring(getClass.getName.lastIndexOf('.') + 1)
+  val name = simpleName.substring(0, simpleName.length - 1) match {
     case s => s.substring(s.lastIndexOf('$') + 1)
   }
 }
