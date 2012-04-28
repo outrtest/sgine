3e9a3a47d9a79194d2889d00c87b64b8128822fb
Matt Hicks <matt@outr.com>
2012-04-28 13:15:24 -0500

    Moved org.sgine.ui.easing to core project.

--- core/src/main/scala/org/sgine/easing/Sine.scala
+++ core/src/main/scala/org/sgine/easing/Sine.scala
@@ -1,15 +1,17 @@
-package org.sgine.ui.easing
+package org.sgine.easing
 
-object Linear {
+import scala.math._
+
+object Sine {
   def easeIn(time: Double, start: Double, change: Double, duration: Double): Double = {
-    change * time / duration + start
+    -change * cos(time / duration * (Pi / 2.0)) + change + start
   }
 
   def easeOut(time: Double, start: Double, change: Double, duration: Double): Double = {
-    change * time / duration + start
+    change * sin(time / duration * (Pi / 2.0)) + start
   }
 
   def easeInOut(time: Double, start: Double, change: Double, duration: Double): Double = {
-    change * time / duration + start
+    -change / 2.0 * (cos(Pi * time / duration) - 1.0) + start
   }
 }
