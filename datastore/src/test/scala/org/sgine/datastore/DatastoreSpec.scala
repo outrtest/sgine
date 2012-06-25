716df1ae0840df71d8aed7720ffa3f1465305f1e
Matt Hicks <matt@outr.com>
2012-06-25 10:51:10 -0500

    Finished DatastoreSpec updates to work with query and removed casbah dependency from SgineBuild.

--- datastore/src/test/scala/org/sgine/datastore/DatastoreSpec.scala
+++ datastore/src/test/scala/org/sgine/datastore/DatastoreSpec.scala
@@ -43,7 +43,6 @@ class DatastoreSpec extends WordSpec with ShouldMatchers {
 //    }
     "using mongodb" should {
       val datastore = new MongoDBDatastore("localhost", 27017, "DatastoreSpec")
-      datastore.globalize = true
       val (session, created) = datastore.createOrGet()
       datastore.session.dropDatabase()
       test(session) {
@@ -56,27 +55,26 @@ class DatastoreSpec extends WordSpec with ShouldMatchers {
   }
 
   def test(session: DatastoreSession)(finish: => Unit) = {
-    val db1 = session[Test1]
-    val db2 = session[Test2]
-    val db3 = session[Test3]
-    val db4 = session[Test4]
+    val c1 = session[Test1]
+    val c2 = session[Test2]
+    val c3 = session[Test3]
     println("Within the session...")
     val t1 = Test1("test1")
     "have no objects in the database" in {
       println("Checking db size...")
-      db1.size should equal(0)
+      c1.size should equal(0)
     }
     "insert an object" in {
-      db1.persist(t1)
+      c1.persist(t1)
     }
     "query the object back out" in {
-      val results = db1.toList
+      val results = c1.toList
       results.size should equal(1)
       results.head should equal(t1)
     }
     "delete the object" in {
-      db1.delete(t1)
-      db1.size should equal(0)
+      c1.delete(t1)
+      c1.size should equal(0)
     }
     "insert five Test1 objects" in {
       val o1 = Test1("One")
@@ -84,10 +82,10 @@ class DatastoreSpec extends WordSpec with ShouldMatchers {
       val o3 = Test1("Three")
       val o4 = Test1("Four")
       val o5 = Test1("Five")
-      db1.persist(o1, o2, o3, o4, o5)
+      c1.persist(o1, o2, o3, o4, o5)
     }
     "query 'Three' back out" in {
-      val query = db1.query.filter(Test1.name equal "Three")
+      val query = c1.query.filter(Test1.name equal "Three")
       val results = query.toList
       results.size should equal(1)
       results.head.name should equal("Three")
@@ -98,35 +96,26 @@ class DatastoreSpec extends WordSpec with ShouldMatchers {
       val o3 = Test2("Three")
       val o4 = Test2("Four")
       val o5 = Test2("Five")
-      db2.persist(o1, o2, o3, o4, o5)
+      c2.persist(o1, o2, o3, o4, o5)
     }
     "properly differentiate between class types via all" in {
-      db1.size should equal(5)
-      db2.size should equal(5)
+      c1.size should equal(5)
+      c2.size should equal(5)
     }
     "property differentiate between class types via query" in {
-      val results1 = db1.query.filter(Test1.name equal "Three").toList
-      val results2 = db2.query.filter(Test2.name equal "Three").toList
+      val results1 = c1.query.filter(Test1.name equal "Three").toList
+      val results2 = c2.query.filter(Test2.name equal "Three").toList
       results1.size should equal(1)
       results2.size should equal(1)
     }
-    "insert a Test3 that extends Test2" in {
-      db3.persist(Test3("Three"))
+    "persist a Test3 with an EnumEntry" in {
+      c3.persist(Test3("first", Precision.Milliseconds))
     }
-//    "verify Test3 returns with all[Test2]" in {
-//      db2.size should equal(6)
-//    }
-//    "verify Test3 returns with Test2 query" in {
-//      db2.query((t: Test2) => true).size should equal(6)
-//    }
-    "persist a Test4 with an EnumEntry" in {
-      db4.persist(Test4("first", Precision.Milliseconds))
+    "query back one Test3 with an EnumEntry" in {
+      val t = c3.head
+      t.name should equal("first")
+      t.precision should equal(Precision.Milliseconds)
     }
-//    "query back one Test4 with an EnumEntry" in {
-//      val t = db4.first()
-//      t.name should equal("first")
-//      t.precision should equal(Precision.Milliseconds)
-//    }
     "close resources in" in {
       finish
     }
@@ -147,17 +136,10 @@ object Test2 {
   val id = Field.id[Test2]
 }
 
-case class Test3(override val name: String, override val id: UUID = UUID.randomUUID()) extends Test2(name, id)
+case class Test3(name: String, precision: Precision, id: UUID = UUID.randomUUID()) extends Persistable
 
 object Test3 {
   val name = Field[Test3, String]("name")
+  val precision = Field[Test3, Precision]("precision")
   val id = Field.id[Test3]
-}
-
-case class Test4(name: String, precision: Precision, id: UUID = UUID.randomUUID()) extends Persistable
-
-object Test4 {
-  val name = Field[Test4, String]("name")
-  val precision = Field[Test4, Precision]("precision")
-  val id = Field.id[Test4]
 }
